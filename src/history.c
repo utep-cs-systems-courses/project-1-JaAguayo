44d72e11d97a81fccae67ff930c18dbac3d0a8be
JaAguayo <jmaguayo@miners.utep.edu>
2020-06-26 12:15:28 -0600

    first history version

--- src/history.c
+++ src/history.c
@@ -4,9 +4,44 @@
 #include "history.h"
 
 List* init_history(){
+  List newList-> root = (Item) malloc (sizeof(Item));
+  return newList;
+}
+//after calling this once do a temp in main with and do word term and send it a new pointer
+void add_history(List *list, char *str){
+  int len = len_word(str);
+  int i,count;
   
+  if (*root == 0){
+    char *string_copy = copy_str(str,len);
+    Item newNode->str = string_copy;
+    newNode->id = 1;
+    list->root = newNode;
+    list->root->newNode->next = NULL;
+  }
+  else{
+    Item current = (list->root);
+    while (current->next != NULL){
+      currnt = current->next;
+      ++count;
+    }
+    char *string_copy = copy_str(str,len);
+    current->next = (Item) malloc (sizeof(Item));
+    current->next->id = (count+1);
+    current->next->str = string_copy;
+    current->next->next = NULL;
+    
+  }
 }
 
-void add_history(List *list, char *str){
-  
+char *get_history(List *list){
+  ;
+}
+
+void print_history(List *list){
+  ;
+}
+
+void free_history(List *list){
+  ;
 }
